1cd3c5a581944c871075e216d5600eed6fde0426
Felipe Ortega <glimmerphoenix@gmail.com>
2014-04-01 01:23:33 +0200

    Template of a class modeling logged users who edit pages

---
+++ wikidat/sources/user.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 31 18:02:54 2014
+
+@author: jfelipe
+"""
+
+from wikidat.utils import dbutils
+from data_item import DataItem
+
+
+class User(DataItem):
+    """
+    Models Page elements in Wikipedia database dumps
+    """
+
+    def __init__(self, data_dict={}, lang=None):
+        """
+        Constructor method for User objects. Must forward params to
+        parent class DataItem (mandatory inheritance)
+
+        Parameters
+        ----------
+        data_dict: dictionary of raw data to be processed
+        lang: identifier of Wikipedia language edition from which this
+        element comes from (e.g. frwiki, eswiki, dewiki...)
+        """
+        super(User, self).__init__(data_dict=data_dict, lang=lang)
