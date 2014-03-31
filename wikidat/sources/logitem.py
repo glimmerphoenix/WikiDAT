f72ebb483bf3ddbbf29237370f977eabd9accd6d
Felipe Ortega <glimmerphoenix@gmail.com>
2014-04-01 01:24:28 +0200

    Template of a class modeling logitem elements from 'logging' table in MediaWiki DB schema

---
+++ wikidat/sources/logitem.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 29 22:14:21 2014
+
+@author: jfelipe
+"""
+from data_item import DataItem
+
+
+class LogItem(DataItem):
+    """
+    Models LogItem elements extracted from the 'logging' DB table in Wikipedia
+    """
+
+    def __init__(self, data_dict, lang):
+        """
+        Constructor method for Page objects. Must forward params to
+        parent class DataItem (mandatory inheritance)
+
+        Parameters
+        ----------
+        data_dict: dictionary of raw data to be processed
+        lang: identifier of Wikipedia language edition from which this
+        element comes from (e.g. frwiki, eswiki, dewiki...)
+        """
+        super(LogItem, self).__init__(data_dict=data_dict, lang=lang)
