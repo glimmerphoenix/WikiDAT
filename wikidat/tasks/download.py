7556611393ae3d9d36069995e07263978bbae316
Felipe Ortega <glimmerphoenix@gmail.com>
2014-05-12 18:42:40 +0200

    Downloaders and integrity checkers for common dump files

--- wikidat/tasks/download.py
+++ wikidat/tasks/download.py
@@ -16,7 +16,16 @@ import os
 import hashlib
 
 
-class RevisionDownloader(object):
+class Downloader(object):
+    """
+    Download manager for any type of Wikipedia dump file
+    Subclasses will instantiate methods to deal with the specific tasks
+    to download each type of dump file
+    """
+    pass
+
+
+class RevDownloader(object):
     """
     Downloads revision dump files from http://dumps.wikimedia.org
     There are different instances of revision dumps (metadata only, complete
@@ -41,10 +50,13 @@ class RevisionDownloader(object):
         self.dump_paths = []  # List of paths to dumps in local filesystem
         self.md5_codes = {}  # Dict for md5 codes to verify dump files
 
-    def download(self, dump_date):
+    def download(self, dump_date=self.dump_dates[-2]):
         """
         Download all dump files for a given language in their own folder
-        Return
+        Return list of paths to dump files to be processed
+
+        Default target dump_date is latest available dump (index -2, as the
+        last item is the generic 'latest' date, not a real date)
         """
         # Obtain content for dump summary page on requested date
         base_url = "".join([self.target_url, "/", dump_date])
@@ -130,19 +142,20 @@ class RevisionDownloader(object):
                 pass
 
 
-class RevisionHistoryDownloader(RevisionDownloader):
+class RevHistDownloader(RevDownloader):
     """
     Downloads revision history files from http://dumps.wikimedia.org
     These are files with complete revision history information (all text)
     """
 
     def __init__(self, mirror, language):
-        super(RevisionHistoryDownloader, self).__init__(mirror=mirror,
-                                                        language=language)
+        super(RevHistDownloader, self).__init__(mirror=mirror,
+                                                language=language)
+        # Customized pattern to find dump files on mirror server page
         self.match_pattern = 'pages-meta-history[\S]*\.xml\.7z'
 
 
-class RevisionMetaDownloader(RevisionDownloader):
+class RevMetaDownloader(RevDownloader):
     """
     Downloads revision meta files from http://dumps.wikimedia.org
     These are files with complete metadata for every revision (including
@@ -150,6 +163,23 @@ class RevisionMetaDownloader(RevisionDownloader):
     """
 
     def __init__(self, mirror, language):
-        super(RevisionHistoryDownloader, self).__init__(mirror=mirror,
-                                                        language=language)
+        super(RevMetaDownloader, self).__init__(mirror=mirror,
+                                                language=language)
+# Customized pattern to find dump files on mirror server page
         self.match_pattern = 'stub-meta-history[\d]+\.xml\.gz'
+
+
+class LoggingDownloader(Downloader):
+    """
+    Download logging dump files
+    """
+    pass
+
+
+class SQLDumpDownloader(Downloader):
+    """
+    Download compressed or uncompressed sql dump files ready to be directly
+    loaded to the local database
+    """
+    pass
+    
