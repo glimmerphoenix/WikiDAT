f2763dba00269f30dd9305768b302547b1fcb9ed
Felipe Ortega <glimmerphoenix@gmail.com>
2014-05-14 17:47:08 +0200

    Adding missing package id file

---
+++ wikidat/tasks/__init__.py
@@ -0,0 +1,11 @@
+"""
+wikidat.tasks
+
+Modules implementing the entire workflow for data analytics:
+    - ETL
+        - Extraction
+        - Transformation
+        - Loading
+    - Analysis/modeling
+    - Visualization
+"""
