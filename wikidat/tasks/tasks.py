948f04489c68822d13e1eba39c604d987a98911e
Felipe Ortega <glimmerphoenix@gmail.com>
2014-05-21 20:18:57 +0200

    Support for multiple etl processes simultaneously

--- wikidat/tasks/tasks.py
+++ wikidat/tasks/tasks.py
@@ -15,6 +15,7 @@ processes with Wikipedia data:
 
 from wikidat.sources.etl import PageRevisionETL
 from download import RevHistDownloader
+import multiprocessing as mp
 
 
 class Task(object):
@@ -65,10 +66,36 @@ class RevisionHistoryTask(Task):
 
         db_name = self.lang + '_' + self.date.strip('/')
         print "paths: " + unicode(self.paths)
-        self.etl = PageRevisionETL(paths=self.paths, lang=self.lang,
-                                   page_fan=page_fan, rev_fan=rev_fan,
-                                   db_name=db_name,
-                                   db_user=db_user, db_passw=db_passw)
+
+        # TODO: Retrieve information about available CPU cores and
+        # Number of parallel ETL processes to be started to maximize
+        # system's throughput
+
+        if len(self.paths) > 1:
+            # Case of multiple paths
+            paths1 = self.paths[0:len(self.paths)/2]
+            paths2 = self.paths[len(self.paths)/2:]
+            self.etl1 = PageRevisionETL(paths=paths1, lang=self.lang,
+                                        page_fan=page_fan, rev_fan=rev_fan,
+                                        db_name=db_name,
+                                        db_user=db_user, db_passw=db_passw)
+            self.etl2 = PageRevisionETL(paths=paths2, lang=self.lang,
+                                        page_fan=page_fan, rev_fan=rev_fan,
+                                        db_name=db_name,
+                                        db_user=db_user, db_passw=db_passw)
+            proc_etl1 = mp.Process(target=self.etl1.run())
+            proc_etl1.start()
+            proc_etl2 = mp.Process(target=self.etl2.run())
+            proc_etl2.start()
+            proc_etl1.join()
+            proc_etl2.join()
+
+        else:
+            # Case of single path
+            self.etl = PageRevisionETL(paths=self.paths, lang=self.lang,
+                                       page_fan=page_fan, rev_fan=rev_fan,
+                                       db_name=db_name,
+                                       db_user=db_user, db_passw=db_passw)
         print "ETL process for page and revision history defined OK."
         print "Proceeding with ETL workflow. This may take time..."
         # Extract, process and load information in local DB
