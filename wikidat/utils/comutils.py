5ec3c67661baa4ba9b05cc4af0661a8e8818f09b
Felipe Ortega <glimmerphoenix@gmail.com>
2014-06-03 12:11:15 +0200

    Compressing messages using zlib to save some bandwith (in large messages)

--- wikidat/utils/comutils.py
+++ wikidat/utils/comutils.py
@@ -5,14 +5,18 @@ Created on Thu May 29 00:33:50 2014
 @author: jfelipe
 """
 import ujson
+import zlib
 
 
 def send_ujson(socket, obj, flags=0):
     """Serialize object using ultra-fast ujson"""
-    return socket.send(ujson.dumps(obj), flags=flags)
+    m = ujson.dumps(obj)
+    z = zlib.compress(m)
+    return socket.send(z, flags=flags)
 
 
 def recv_ujson(socket, flags=0):
     """Load object from ujson serialization"""
-    obj = socket.recv(flags)
-    return ujson.loads(obj)
+    z = socket.recv(flags)
+    m = zlib.decompress(z)
+    return ujson.loads(m)
