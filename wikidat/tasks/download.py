8b3ac075dab0bb43c36aee4b2072e61be4329d69
Felipe Ortega <glimmerphoenix@gmail.com>
2014-11-24 12:23:22 +0100

    simplify error class

--- wikidat/tasks/download.py
+++ wikidat/tasks/download.py
@@ -11,18 +11,12 @@ import multiprocessing as mp
 import itertools
 import requests
 import re
-import sys
 import os
 import hashlib
 import logging
 
 
-class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-
-class DumpIntegrityError(Error):
+class DumpIntegrityError(Exception):
     """Exception raised for errors in the input.
 
     Attributes:
